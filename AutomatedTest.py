41e82d353b041950f5f2095ed47f4cef7f8f577f
mfirat <muratfirat78@gmail.com>
2026-04-10 18:58:35 +0200

    Update AutomatedTest.py

--- AutomatedTest.py
+++ AutomatedTest.py
@@ -100,7 +100,7 @@ def finalize(student_name,student_no,uploaded_files):
     http_with_timeout = httplib2.Http(timeout=3600)
     authed_http = AuthorizedHttp(credentials, http=http_with_timeout)
 
-    drive_service = build('drive', 'v3')
+    drive_service = build('drive', 'v3',http=authed_http)
 
     fid = '1IU1biWrDLZ7J7hWaZRJrQDCaFQPvlc1r'
     
